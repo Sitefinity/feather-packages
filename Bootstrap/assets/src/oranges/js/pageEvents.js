cec544bb25961ea6e8d1cfd6e48a0df5c937f302
Nikolay Stefanov <nstefano@progress.com>
2016-06-30 14:48:55 +0300

    removed custom styles

--- Bootstrap/assets/src/oranges/js/pageEvents.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $(".js-nav-fixed").fixit({
-        top: 0,
-        fixClass: "is-fixed",
-        dummyClass: "fixed-dummy",
-        classOnly: true,
-        renderDummy: true
-    });
-});
