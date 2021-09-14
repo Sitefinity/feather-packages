297d72d7387be88e4ea4d2837f21ae359c60d01b
SFBuild <sfbuild@progress.com>
2021-09-14 14:16:36 +0300

    Release v.14.0.7700.0

--- Bootstrap4/MVC/Scripts/Navigation/pills.js
+++ Bootstrap4/MVC/Scripts/Navigation/pills.js
@@ -1,30 +1,43 @@
 ﻿(function () {
-    /* Polyfills */
+/* Polyfills */
 
     if (window.NodeList && !NodeList.prototype.forEach) {
         NodeList.prototype.forEach = Array.prototype.forEach;
     }
 
-    document.addEventListener('DOMContentLoaded', function () {
-        document.querySelectorAll('.expandable-node').forEach(function (x) {
-            x.addEventListener('click', function (e) {
-                if (window.DataIntelligenceSubmitScript && e.currentTarget.hasAttribute('aria-expanded') === 'false') {
-                    DataIntelligenceSubmitScript._client.sentenceClient.writeSentence({
-                        predicate: "Toggle navigation",
-                        object: e.currentTarget.innerText.trim(),
-                        objectMetadata: [{
-                            'K': 'PageTitle',
-                            'V': document.title
-                        },
-                        {
-                            'K': 'PageUrl',
-                            'V': location.href
-                        }
-                        ]
-                    });
-                }
+    var initialized = false;
+    var initialize = function () {
+        if (!initialized) {
+            initialized = true;
+            document.querySelectorAll('.expandable-node').forEach(function (x) {
+                x.addEventListener('click', function (e) {
+                    if (window.DataIntelligenceSubmitScript && e.currentTarget.hasAttribute('aria-expanded') === 'false') {
+                        DataIntelligenceSubmitScript._client.sentenceClient.writeSentence({
+                            predicate: "Toggle navigation",
+                            object: e.currentTarget.innerText.trim(),
+                            objectMetadata: [{
+                                'K': 'PageTitle',
+                                'V': document.title
+                            },
+                            {
+                                'K': 'PageUrl',
+                                'V': location.href
+                            }
+                            ]
+                        });
+                    }
+                });
             });
-        });
+        }
+    };
+
+    document.addEventListener('DOMContentLoaded', function () {
+        initialize();
     });
 
+    if (window.personalizationManager) {
+        window.personalizationManager.addPersonalizedContentLoaded(function () {
+            initialize();
+        });
+    }
 }());
