0ffb8f7708610d09940de3b5a070ad8ac79b066b
SFBuild <sfbuild@progress.com>
2021-11-18 11:58:15 +0200

    Release v.14.0.7723.0

--- Bootstrap4/MVC/Scripts/Navigation/pills.js
+++ Bootstrap4/MVC/Scripts/Navigation/pills.js
@@ -1,20 +1,43 @@
-(function ($) {
-    $('.expandable-node').on('click', function (item) {
-        var target = $(item.currentTarget);
-        if (window.DataIntelligenceSubmitScript && target.attr('aria-expanded') === 'false') {
-            DataIntelligenceSubmitScript._client.sentenceClient.writeSentence({
-                predicate: "Toggle navigation",
-                object: target.text().trim(),
-                objectMetadata: [{
-                    'K': 'PageTitle',
-                    'V': document.title
-                },
-                {
-                    'K': 'PageUrl',
-                    'V': location.href
-                }
-                ]
+﻿(function () {
+/* Polyfills */
+
+    if (window.NodeList && !NodeList.prototype.forEach) {
+        NodeList.prototype.forEach = Array.prototype.forEach;
+    }
+
+    var initialized = false;
+    var initialize = function () {
+        if (!initialized) {
+            initialized = true;
+            document.querySelectorAll('.expandable-node').forEach(function (x) {
+                x.addEventListener('click', function (e) {
+                    if (window.DataIntelligenceSubmitScript && e.currentTarget.hasAttribute('aria-expanded') === 'false') {
+                        DataIntelligenceSubmitScript._client.sentenceClient.writeSentence({
+                            predicate: "Toggle navigation",
+                            object: e.currentTarget.innerText.trim(),
+                            objectMetadata: [{
+                                'K': 'PageTitle',
+                                'V': document.title
+                            },
+                            {
+                                'K': 'PageUrl',
+                                'V': location.href
+                            }
+                            ]
+                        });
+                    }
+                });
             });
         }
-    })
-}(jQuery));
+    };
+
+    document.addEventListener('DOMContentLoaded', function () {
+        initialize();
+    });
+
+    if (window.personalizationManager) {
+        window.personalizationManager.addPersonalizedContentLoaded(function () {
+            initialize();
+        });
+    }
+}());
